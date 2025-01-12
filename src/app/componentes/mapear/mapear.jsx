ee62c7380b4ba45bec6e970898be8fcc92ed77f3
wellmendes25 <wellyson.wm@gmail.com>
2025-01-12 12:02:33 -0300

    responsividade

--- src/app/componentes/mapear/mapear.jsx
+++ src/app/componentes/mapear/mapear.jsx
@@ -1,28 +1,11 @@
 'use client';
-import { useState, useEffect } from 'react';
-import Cookies from 'js-cookie'; 
+import { useState } from 'react';
 import estilos from './mapear.module.css';
 
 export default function Mapear({ NomesALunos }) {
-    const [presente, setPresente] = useState(() => {
-    
-        const savedPresente = Cookies.get('presente');
-        return savedPresente ? JSON.parse(savedPresente) : NomesALunos.map(() => ({ count: 0, dates: [] }));
-    });
-
-    const [falta, setFalta] = useState(() => {
-        
-        const savedFalta = Cookies.get('falta');
-        return savedFalta ? JSON.parse(savedFalta) : NomesALunos.map(() => ({ count: 0, dates: [] }));
-    });
-
+    const [presente, setPresente] = useState(NomesALunos.map(() => ({ count: 0, dates: [] })));
+    const [falta, setFalta] = useState(NomesALunos.map(() => ({ count: 0, dates: [] })));
     const [mostrarDatas, setMostrarDatas] = useState(NomesALunos.map(() => false));
-
-    useEffect(() => {
-        
-        Cookies.set('presente', JSON.stringify(presente));
-        Cookies.set('falta', JSON.stringify(falta));
-    }, [presente, falta]);
 
     function Presente(index) {
         const data = new Date().toLocaleDateString();
@@ -60,8 +43,6 @@ export default function Mapear({ NomesALunos }) {
         const resetData = NomesALunos.map(() => ({ count: 0, dates: [] }));
         setPresente(resetData);
         setFalta(resetData);
-        Cookies.remove('presente');
-        Cookies.remove('falta');
     }
 
     return (
@@ -71,53 +52,55 @@ export default function Mapear({ NomesALunos }) {
             </button>
             {NomesALunos.map((aluno, index) => (
                 <section key={aluno.id}>
-                    <table className={estilos.tabela}>
-                        <thead>
-                            <tr>
-                                <th className={estilos.primeiro}>Nome:</th>
-                                <th>S:</th>
-                                <th>P:</th>
-                                <th>F:</th>
-                                <th>Dias p:</th>
-                                <th>Dias f:</th>
-                                <th className={estilos.tabelaFinal}></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className={estilos.primeiro}>{aluno.nome}</td>
-                                <td>
-                                    <button className={estilos.presente} onClick={() => Presente(index)}>
-                                        P
-                                    </button> - 
-                                    <button className={estilos.falta} onClick={() => Falta(index)}>
-                                        F
-                                    </button>
-                                </td>
-                                <td>{presente[index].count}</td>
-                                <td>{falta[index].count}</td>
-                                <td>
-                                    {mostrarDatas[index] && (
-                                        <div>
-                                            <strong>Presenças:</strong> {presente[index].dates.join(", ")}
-                                        </div>
-                                    )}
-                                </td>
-                                <td>
-                                    {mostrarDatas[index] && (
-                                        <div>
-                                            <strong>Faltas:</strong> {falta[index].dates.join(", ")}
-                                        </div>
-                                    )}
-                                </td>
-                                <td className={estilos.botaoFinal}> 
-                                    <button onClick={() => MostrarDatas(index)} className={estilos.botaoMostrarDatas}>
-                                        &#8592;
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div className={estilos}>
+                        <table className={estilos.tabela}>
+                            <thead>
+                                <tr>
+                                    <th className={estilos.primeiro}>Nome:</th>
+                                    <th>Status:</th>
+                                    <th>Presenças:</th>
+                                    <th>Faltas:</th>
+                                    <th>Dias presentes:</th>
+                                    <th>Dias faltosos:</th>
+                                    <th className={estilos.tabelaFinal}></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className={estilos.primeiro}>{aluno.nome}</td>
+                                    <td>
+                                        <button className={estilos.presente} onClick={() => Presente(index)}>
+                                            P
+                                        </button> -
+                                        <button className={estilos.falta} onClick={() => Falta(index)}>
+                                            F
+                                        </button>
+                                    </td>
+                                    <td>{presente[index].count}</td>
+                                    <td>{falta[index].count}</td>
+                                    <td>
+                                        {mostrarDatas[index] && (
+                                            <div>
+                                                <strong>Presenças:</strong> {presente[index].dates.join(", ")}
+                                            </div>
+                                        )}
+                                    </td>
+                                    <td>
+                                        {mostrarDatas[index] && (
+                                            <div>
+                                                <strong>Faltas:</strong> {falta[index].dates.join(", ")}
+                                            </div>
+                                        )}
+                                    </td>
+                                    <td className={estilos.botaoFinal}>
+                                        <button onClick={() => MostrarDatas(index)} className={estilos.botaoMostrarDatas}>
+                                            &#8592;
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </section>
             ))}
         </>
